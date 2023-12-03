a58bb42bce17dd45897cf0b138e5ee4f43258641
zypherscript <kitti.kthp@gmail.com>
2023-12-03 16:53:46 +0700

    vt

--- src/main/java/com/example/springtipsboot32/Application.java
+++ src/main/java/com/example/springtipsboot32/Application.java
@@ -1,13 +1,78 @@
 package com.example.springtipsboot32;
 
+import java.util.ArrayList;
+import java.util.concurrent.ConcurrentSkipListSet;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
 
   public static void main(String[] args) {
     SpringApplication.run(Application.class, args);
+  }
+
+  @Bean
+  ApplicationRunner loom() {
+    return args -> {
+      var observed = new ConcurrentSkipListSet<String>();
+      var threads = new ArrayList<Thread>();
+      for (int i = 0; i < 1_000; i++) {
+        var ind = i;
+        threads.add(Thread.ofVirtual().unstarted(new Runnable() {
+          @Override
+          public void run() {
+            if (ind == 0) {
+              observed.add(Thread.currentThread().toString());
+            }
+            try {
+              Thread.sleep(100);
+            } catch (InterruptedException e) {
+              throw new RuntimeException(e);
+            }
+            if (ind == 0) {
+              observed.add(Thread.currentThread().toString());
+            }
+
+            try {
+              Thread.sleep(100);
+            } catch (InterruptedException e) {
+              throw new RuntimeException(e);
+            }
+            if (ind == 0) {
+              observed.add(Thread.currentThread().toString());
+            }
+
+            try {
+              Thread.sleep(100);
+            } catch (InterruptedException e) {
+              throw new RuntimeException(e);
+            }
+            if (ind == 0) {
+              observed.add(Thread.currentThread().toString());
+            }
+
+            try {
+              Thread.sleep(100);
+            } catch (InterruptedException e) {
+              throw new RuntimeException(e);
+            }
+            if (ind == 0) {
+              observed.add(Thread.currentThread().toString());
+            }
+          }
+        }));
+      }
+      for (var t : threads) {
+        t.start();
+      }
+      for (var t : threads) {
+        t.join();
+      }
+      System.out.println(observed);
+    };
   }
 
   String displayUserForLoan(Loan loan) {
@@ -37,4 +102,3 @@ final class SecuredLoan implements Loan {
 record UnsecuredLoan(float interest) implements Loan {
 
 }
-
